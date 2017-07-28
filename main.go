c0accec4adf53a317458f69b56ea04d830ad71bc
Guilherme Paixão <guiferpa@gmail.com>
2017-07-28 01:19:58 -0300

    Refactoring the code, writing based on interface

--- main.go
+++ main.go
@@ -1,207 +1,19 @@
 package main
 
-import  (
-	"strconv"
+import (
+	"fmt"
 	"os"
 	"os/signal"
-	"context"
-	"net"
-	"net/url"
-	"net/http"
-	"fmt"
-	"io/ioutil"
-	"encoding/json"
-	"sync"
-	"time"
+	"strconv"
 	"syscall"
+	"time"
+
+	"github.com/stone-payments/gcd/worker"
 )
-
-type (
-	Container struct {
-		Id string
-		Created int
-		SizeRw int
-		Status string
-		State string
-	}
-
-	Image struct {
-		Id string
-	}
-)
-
-func removeImage(wg *sync.WaitGroup, httpc http.Client, i Image) {
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://v1.24/images/%v?force=true", i.Id), nil)
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := httpc.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		fmt.Println("[INFO] Image", i.Id, "removed successful")
-	}
-	wg.Done()
-}
-
-func getImages(cImages chan []Image, httpc http.Client) {
-	
-	req, err := http.NewRequest("GET", "http://v1.24/images/json?all=1", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := httpc.Do(req)
-	defer resp.Body.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	images := make([]Image, 0)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := json.Unmarshal(body, &images); err != nil {
-		panic(err)
-	}
-
-	cImages <- images
-}
-
-func removeContainer(wg *sync.WaitGroup, httpc http.Client, c Container)  {
-	if c.State != "running" {
-		req, err := http.NewRequest("DELETE", fmt.Sprintf("http://v1.24/containers/%v", c.Id), nil)
-		if err != nil {
-			panic(err)
-		}
-
-		resp, err := httpc.Do(req)
-		if err != nil {
-			fmt.Sprintln(err.Error())
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode == http.StatusNoContent {
-			fmt.Println("[INFO] Container", c.Id, "removed successful")
-		}
-	}
-	wg.Done()
-}
-
-func getContainers(cContainers chan []Container, httpc http.Client) {
-	req, err := http.NewRequest("GET", "http://v1.24/containers/json?all=1", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := httpc.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	containers := make([]Container, 0)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := json.Unmarshal(body, &containers); err != nil {
-		panic(err)
-	}
-
-	cContainers <- containers
-}
-
-func newClient(host string) (http.Client, error) {
-	urlParsed, err := url.Parse(host)
-	if err != nil {
-		return http.Client{}, err
-	}
-
-	return http.Client{
-		Transport: &http.Transport{
-			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", urlParsed.Path)
-			},
-		},
-	}, nil
-}
-
-func start(host string, httpc http.Client) {
-	cContainers := make(chan []Container)
-	cImages := make(chan []Image)
-
-	go getContainers(cContainers, httpc)
-	go getImages(cImages, httpc)
-
-	select {
-		// case containers, images := <- cContainers, <- cImages:
-		case containers := <- cContainers:
-			fmt.Println("[INFO] Time:", time.Now().UnixNano())
-			fmt.Println("[INFO] Host:", host)
-			fmt.Println("[INFO] Containers total:", len(containers))
-			select {
-				case images := <- cImages:
-					fmt.Println("[INFO] Images total:", len(images))
-
-					wgContainerDone := make(chan bool)
-					var wgContainer sync.WaitGroup
-					for _, container := range containers {
-						wgContainer.Add(1)
-						go removeContainer(&wgContainer, httpc, container)
-					}
-
-					go func(done chan bool) {
-						wgContainer.Wait()
-						done <- true
-					}(wgContainerDone)
-
-					select {
-						case <-wgContainerDone:
-							fmt.Println("[INFO] Action to containers finished")
-						case <-time.After(time.Second * 1):
-							fmt.Println("[ERROR] Action to containers timeout occurred")
-					}
-
-					wgImageDone := make(chan bool)
-					var wgImage sync.WaitGroup
-					for _, image := range images {
-						wgImage.Add(1)
-						go removeImage(&wgImage, httpc, image)
-					}
-					go func(done chan bool) {
-						wgImage.Wait()
-						done <- true
-					}(wgImageDone)
-
-					select {
-						case <-wgImageDone:
-							fmt.Println("[INFO] Action to images finished")
-						case <-time.After(time.Second * 1):
-							fmt.Println("[ERROR] Action to images timeout occurred")
-					}
-
-				case <-time.After(time.Second * 1):
-					fmt.Println("[ERROR] Images request timeout")
-			}
-
-		case <-time.After(time.Second * 1):
-			fmt.Println("[ERROR] Containers request timeout")
-	}
-}
 
 func main() {
-	
 	interval := 1
+
 	host := os.Getenv("GCD_DOCKER_HOST")
 	if host == "" {
 		host = "unix:///var/run/docker.sock"
@@ -217,7 +29,7 @@ func main() {
 		}
 	}
 
-	httpc, err := newClient(host)
+	worker, err := worker.New(host, "1.24")
 	if err != nil {
 		panic(err)
 	}
@@ -228,12 +40,11 @@ func main() {
 
 	for {
 		select {
-			case <-sig:
-				fmt.Printf("\nDown daemon by signal: %v\n", <-sig)
-				os.Exit(0)
-			case <-time.After(time.Second * time.Duration(interval)):
-				fmt.Println("")
-				start(host, httpc)
+		case <-sig:
+			fmt.Printf("\nDown daemon by signal: %v\n", <-sig)
+			os.Exit(0)
+		case <-time.After(time.Second * time.Duration(interval)):
+			worker.Sweep()
 		}
 	}
 }
---
+++ worker/worker.go
@@ -0,0 +1,221 @@
+package worker
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+
+	"github.com/stone-payments/gcd/logger"
+)
+
+type (
+	Container struct {
+		Id      string
+		Created int
+		SizeRw  int
+		Status  string
+		State   string
+	}
+
+	Image struct {
+		Id string
+	}
+
+	Worker interface {
+		ListContainers(ch chan []Container)
+		ListImages(ch chan []Image)
+		RemoveContainer(wg *sync.WaitGroup, container Container)
+		RemoveImage(wg *sync.WaitGroup, image Image)
+		Sweep()
+	}
+
+	WorkerAsClient struct {
+		conn    http.Client
+		host    string
+		version string
+		logger  logger.Logger
+	}
+)
+
+func (wac WorkerAsClient) ListContainers(ch chan []Container) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://v%v/containers/json?all=1", wac.version), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := wac.conn.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	containers := make([]Container, 0)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(body, &containers); err != nil {
+		panic(err)
+	}
+
+	ch <- containers
+}
+
+func (wac WorkerAsClient) ListImages(ch chan []Image) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://v%v/images/json?all=1", wac.version), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := wac.conn.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	images := make([]Image, 0)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(body, &images); err != nil {
+		panic(err)
+	}
+
+	ch <- images
+}
+
+func (wac WorkerAsClient) RemoveContainer(wg *sync.WaitGroup, container Container) {
+	if container.State != "running" {
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("http://v%v/containers/%v", wac.version, container.Id), nil)
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := wac.conn.Do(req)
+		if err != nil {
+			fmt.Sprintln(err.Error())
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusNoContent {
+			wac.logger.Info("Container", container.Id, "removed successful")
+		}
+	}
+	wg.Done()
+}
+
+func (wac WorkerAsClient) RemoveImage(wg *sync.WaitGroup, image Image) {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://v%v/images/%v?force=true", wac.version, image.Id), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := wac.conn.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		wac.logger.Info("Image", image.Id, "removed successful")
+	}
+	wg.Done()
+}
+
+func (wac WorkerAsClient) Sweep() {
+	fmt.Println("")
+
+	chContainers := make(chan []Container)
+	chImages := make(chan []Image)
+
+	go wac.ListContainers(chContainers)
+	go wac.ListImages(chImages)
+
+	select {
+	case containers := <-chContainers:
+		wac.logger.Info("Time:", time.Now().UnixNano())
+		wac.logger.Info("Host:", wac.host)
+		wac.logger.Info("Containers total:", len(containers))
+		select {
+		case images := <-chImages:
+			wac.logger.Info("Images total:", len(images))
+
+			wgContainerDone := make(chan bool)
+			var wgContainer sync.WaitGroup
+			for _, container := range containers {
+				wgContainer.Add(1)
+				go wac.RemoveContainer(&wgContainer, container)
+			}
+
+			go func(done chan bool) {
+				wgContainer.Wait()
+				done <- true
+			}(wgContainerDone)
+
+			select {
+			case <-wgContainerDone:
+				wac.logger.Info("Action to containers finished")
+			case <-time.After(time.Second * 1):
+				wac.logger.Error("Action to containers timeout occurred")
+			}
+
+			wgImageDone := make(chan bool)
+			var wgImage sync.WaitGroup
+			for _, image := range images {
+				wgImage.Add(1)
+				go wac.RemoveImage(&wgImage, image)
+			}
+
+			go func(done chan bool) {
+				wgImage.Wait()
+				done <- true
+			}(wgImageDone)
+
+			select {
+			case <-wgImageDone:
+				wac.logger.Info("Action to images finished")
+			case <-time.After(time.Second * 1):
+				wac.logger.Error("Action to images timeout occurred")
+			}
+
+		case <-time.After(time.Second * 1):
+			wac.logger.Error("Images request timeout")
+		}
+
+	case <-time.After(time.Second * 1):
+		wac.logger.Error("Containers request timeout")
+	}
+}
+
+func New(host, version string) (Worker, error) {
+	urlParsed, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+
+	conn := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", urlParsed.Path)
+			},
+		},
+	}
+
+	return WorkerAsClient{
+		conn,
+		host,
+		version,
+		logger.New(),
+	}, nil
+}
