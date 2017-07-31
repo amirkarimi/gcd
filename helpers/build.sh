dae3929d38922cc36116e945a5c5cb1e4ca095b9
Guilherme Paixão <guiferpa@gmail.com>
2017-07-31 14:07:05 -0300

    Create integration script to Travis

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,5 +1,16 @@
 if which docker >/dev/null ; then
+    mkdir -p ./tmp/build
+    docker run --rm \
+        --user $(id -u):$(id -g) \
+        -e GOPATH=$GOPATH \
+        -e USERNAME=$USERNAME \
+        -e CGO_ENABLED=0 \
+        -v $(pwd):$(pwd) \
+        -w $(pwd) \
+    golang:1.8-onbuild go build
+    mv ./gcd ./tmp/build/
     docker build -t gcd .
+   rm -rf ./tmp
     exit 0;
 fi
 
