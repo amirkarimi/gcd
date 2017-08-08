cd39087a141a2e04e456f2826c114b839f7dad3b
Guilherme Paixão <guiferpa@gmail.com>
2017-08-08 19:06:39 -0300

    Start create tests

--- worker/worker_test.go
+++ worker/worker_test.go
@@ -1,50 +1,173 @@
 package worker
 
 import (
-	"os"
-	"strconv"
+	"errors"
+	"reflect"
 	"testing"
-)
+	"time"
 
-var (
-	sock    = "/var/run/docker.sock"
-	version = "1.24"
+	"github.com/stone-payments/gcd/clients/docker"
+	"github.com/stone-payments/gcd/logger"
+	dockertest "github.com/stone-payments/gcd/tests/clients/docker"
+	loggertest "github.com/stone-payments/gcd/tests/logger"
 )
 
 func TestListContainers(t *testing.T) {
-	worker, err := New(sock, version)
-	if err != nil {
-		panic(err)
+
+	cases := []struct {
+		Expected          []docker.Container
+		FakeGetContainers func() ([]docker.Container, error)
+	}{
+		{
+			Expected: make([]docker.Container, 0),
+			FakeGetContainers: func() ([]docker.Container, error) {
+				return make([]docker.Container, 0), nil
+			},
+		},
+		{
+			Expected: make([]docker.Container, 5),
+			FakeGetContainers: func() ([]docker.Container, error) {
+				return make([]docker.Container, 5), nil
+			},
+		},
+		{
+			Expected: nil,
+			FakeGetContainers: func() ([]docker.Container, error) {
+				return nil, nil
+			},
+		},
+		{
+			Expected: nil,
+			FakeGetContainers: func() ([]docker.Container, error) {
+				return nil, errors.New("Fake error")
+			},
+		},
 	}
 
-	ch := make(chan []Container)
+	for _, test := range cases {
 
-	go worker.ListContainers(ch)
+		dockerClient := dockertest.Client{
+			FakeGetContainers: test.FakeGetContainers,
+		}
 
-	containers := <-ch
+		w := New(dockerClient, loggertest.Logger{}, false, false)
 
-	result := len(containers)
-	expected, err := strconv.Atoi(os.Getenv("GCD_TEST_AMOUNT_CONTAINERS"))
-	if err != nil {
-		panic(err)
+		ch := make(chan []docker.Container)
+		go w.ListContainers(ch)
+
+		select {
+		case containers := <-ch:
+			if !reflect.DeepEqual(test.Expected, containers) {
+				t.Errorf("Not equals containers result: expected: %v, result: %v", test.Expected, containers)
+			}
+		case <-time.After(time.Second * 4):
+			t.Error("Timeout test GetContainers")
+		}
+
 	}
 
-	if result != expected {
-		t.Errorf("worker.ListContainers, result: %v, expected: %v", result, expected)
-	}
 }
 
 func BenchmarkListContainers(b *testing.B) {
-	b.ResetTimer()
-	b.ReportAllocs()
-	worker, err := New(sock, version)
-	if err != nil {
-		panic(err)
-	}
 
-	ch := make(chan []Container)
+	b.ResetTimer()
+
+	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		go worker.ListContainers(ch)
+		dockerClient := dockertest.Client{
+			FakeGetContainers: func() ([]docker.Container, error) {
+				return make([]docker.Container, 0), nil
+			},
+		}
+
+		w := New(dockerClient, logger.New(), false, false)
+
+		ch := make(chan []docker.Container)
+		go w.ListContainers(ch)
+
+		select {
+		case <-ch:
+		}
+	}
+}
+
+func TestListImages(t *testing.T) {
+
+	cases := []struct {
+		Expected      []docker.Image
+		FakeGetImages func() ([]docker.Image, error)
+	}{
+		{
+			Expected: make([]docker.Image, 0),
+			FakeGetImages: func() ([]docker.Image, error) {
+				return make([]docker.Image, 0), nil
+			},
+		},
+		{
+			Expected: make([]docker.Image, 5),
+			FakeGetImages: func() ([]docker.Image, error) {
+				return make([]docker.Image, 5), nil
+			},
+		},
+		{
+			Expected: nil,
+			FakeGetImages: func() ([]docker.Image, error) {
+				return nil, nil
+			},
+		},
+		{
+			Expected: nil,
+			FakeGetImages: func() ([]docker.Image, error) {
+				return nil, errors.New("Fake error")
+			},
+		},
+	}
+
+	for _, test := range cases {
+
+		dockerClient := dockertest.Client{
+			FakeGetImages: test.FakeGetImages,
+		}
+
+		w := New(dockerClient, loggertest.Logger{}, false, false)
+
+		ch := make(chan []docker.Image)
+		go w.ListImages(ch)
+
+		select {
+		case images := <-ch:
+			if !reflect.DeepEqual(test.Expected, images) {
+				t.Errorf("Not equals images result: expected: %v, result: %v", test.Expected, images)
+			}
+		case <-time.After(time.Second * 4):
+			t.Error("Timeout test GetImages")
+		}
+
+	}
+
+}
+
+func BenchmarkListImages(b *testing.B) {
+
+	b.ResetTimer()
+
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		dockerClient := dockertest.Client{
+			FakeGetImages: func() ([]docker.Image, error) {
+				return make([]docker.Image, 0), nil
+			},
+		}
+
+		w := New(dockerClient, logger.New(), false, false)
+
+		ch := make(chan []docker.Image)
+		go w.ListImages(ch)
+
+		select {
+		case <-ch:
+		}
 	}
 }
